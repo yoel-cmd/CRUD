35bbaf3ec5188f7c052e6511eac5dbe2050487cc
yoel ider <yoeli5746@gmail.com>
2025-07-01 19:01:24 +0300

    finish C,U,D

---
+++ db/app.js
@@ -0,0 +1,227 @@
+import { readFile, writeFile } from "node:fs";
+import readline from 'readline';
+
+let maxId=0;
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function WriteFIleFunc(answer) {
+     
+    readFile('db.txt', 'utf8', (err, data) => {
+        if (err) return console.log(err);
+
+        const arr = JSON.parse(data);
+        if(arr.length===0){
+            answer.id=1
+        }
+        else{
+            maxId=0
+            for(let i=0;i<arr.length;i++){
+               if(arr[i].id>maxId){
+                    maxId=arr[i].id                
+               }             
+            }
+            answer.id=maxId+1
+        }
+        arr.push(answer);
+
+        writeFile('db.txt', JSON.stringify(arr,null,2), (err) => {
+            if (err) console.log(err);
+            else console.log('yes');
+             
+        });
+    });
+}
+
+function readFileFunc() {
+    readFile('db.txt', 'utf8', (err, data) => {
+        if (err) return console.log(err);
+        console.log(data); 
+    });
+}
+
+function updateFunctio(params){
+    return new Promise((resolve,reject)=>{
+        readFile('db.txt','utf8',(err,data)=>{
+            const arr= JSON.parse(data);
+             let found = false;
+
+            for(let i=0;i<arr.length;i++){
+                if(arr[i].id===params.id){
+                    arr[i].score=params.score;
+                    found = true;
+                    break;
+                }
+              
+            }
+              if (!found) {
+                return reject('ID not found');
+            }
+            writeFile('db.txt', JSON.stringify(arr,null,2), (err) => {
+            if (err) console.log(err);
+            resolve('Score updated');
+             
+            });
+        })
+    })
+}
+
+function handleChoice(choice) {
+    switch (choice) {
+        case '1':
+            rl.question('(key:value,score:value): ', (answer) => {               
+                    const res = JSON.parse(answer);
+                    WriteFIleFunc(res);                
+                    showMenu();
+            });
+            break;
+        case '2':
+            console.log('read');
+            readFileFunc();
+            showMenu();
+            break;
+        case '3':
+            rl.question('enter id and score: ', (answer) => {  
+                 const res = JSON.parse(answer);                         
+                    updateFunctio(res)
+                    .then((msg)=>{
+                        console.log(msg);
+                        showMenu();
+                    })
+                    .catch((err)=>{
+                        console.log(err);
+                        showMenu();    
+                    })
+            });
+            break;
+        case '4':
+            console.log('delte');
+            showMenu();
+            break;
+        case '0':
+            console.log('exit');
+            rl.close();
+            break;
+        default:
+            console.log('');
+            showMenu();
+            break;
+    }
+}
+
+function showMenu() {
+    console.log('\n========== menu ==========');
+    console.log('1 - creat');
+    console.log('2 - read');
+    console.log('3 - update');
+    console.log('4 - delete');
+    console.log('0 - exit');
+    rl.question('\nenter your chois : ', handleChoice);
+}
+
+
+showMenu();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------
+// import { log } from "node:console";
+// import { readdir, readFile, stat, writeFile } from "node:fs";
+// import readline from 'readline';
+
+// let index = 0;
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+
+
+
+// function WriteFIleFunc(answer) {
+//     readFile('db.txt', 'utf8', (err, data) => {
+//         if (err) console.log(err);
+//         const arr = JSON.parse(data)
+//         console.log(index);
+//         index += 1;
+
+//         arr.push(answer);
+//         console.log('arr.push(answer)');
+//         console.log(arr);
+
+//         writeFile('db.txt', JSON.stringify(arr, null, 2), (err) => {
+//             if (err) {
+//                 console.log(err);
+//             }
+//             else {
+//                 console.log('yes');
+//             }
+//         })
+//         // console.log(data); 
+//     })
+// }
+
+
+// function readFileFunc() {
+//     readFile('db.txt', 'utf8', (err, data) => {
+//         if (err) console.log(err);
+//         console.log(data);
+//     })
+// }
+
+
+// function handleChoice(choice) {
+//     while(choice!=0){
+//     switch (choice) {
+//         case '1':
+//             rl.question('Insert json Key:Value ', (answer) => {
+//                 const res = JSON.parse(answer)
+//                 rl.close();
+//                 WriteFIleFunc(res)
+//             });
+//             break;
+//         case '2':
+//             console.log(' create');
+//             // כאן תקרא את הקובץ ותדפיס את התוכן
+//             break;
+//         case '3':
+//             console.log(' update');
+//             // כאן תבקש מזהה ותשנה את הרשומה
+//             break;
+//         case '4':
+//             console.log('delete');
+//             // כאן תבקש מזהה ותמחק את הרשומה
+//             break;
+//             case '0':
+//             console.log('exit');
+//             // כאן תבקש מזהה ותמחק את הרשומה
+//             break;
+//         default:
+//             console.log('בחירה לא חוקית, נסה שוב');
+//             showMenu(); // חזרה לתפריט במקרה של שגיאה
+//             return;
+//     }
+    
+
+    
+// }
+
+// }
